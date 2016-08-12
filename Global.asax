c2c22d32f6f22f28122dce1126d38e226a83c7c9
Moazzam Ali <moazzamali06@gmail.com>
2016-08-12 10:31:24 -0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Search4Best.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Search4Best.Startup))]
+namespace Search4Best
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.MapSignalR("/signalr",new HubConfiguration()); ///// to configure signalR hubs
+            ConfigureAuth(app);
+        }
+    }
+}
